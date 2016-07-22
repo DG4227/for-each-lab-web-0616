aa8877fe3646dd5eb92ebc723957c2ea1b00f0c1
Dan Gorman <danielhgorman@gmail.com>
2016-07-22 10:24:36 -0400

    Done.

---
+++ forEach.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  array.forEach((el, i) => {
+    console.log(`${i}: ${el}`)
+  })
+}
+
+function iterate(callback) {
+	array = [1, 2, 3, 4]
+	array.forEach(callback)
+	return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+};
